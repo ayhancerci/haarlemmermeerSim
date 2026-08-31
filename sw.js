e205841141ebd62f5157e8fe94f2e42f05c2b87c
ayhancerci <ayhancerci@gmail.com>
2026-08-31 20:30:03 +0200

    Deploy from source 19f7d9a (sw hm-ea0ee99c7a86)

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * taped glass out of this cache, under the new text. Corrected art that cannot
  * reach the people who saw the wrong art is not a correction.
  */
-const VERSION = 'hm-72fdfe7f968a'
+const VERSION = 'hm-ea0ee99c7a86'
 const SHELL = `${VERSION}-shell`
 const RUNTIME = `${VERSION}-runtime`
 
