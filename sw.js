2b080938a1742fe37a0d15753a7d76c99ba31a2f
ayhancerci <ayhancerci@gmail.com>
2026-09-03 17:02:30 +0200

    Deploy from source ecc43ea (sw hm-d4ad00155bf7)

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * taped glass out of this cache, under the new text. Corrected art that cannot
  * reach the people who saw the wrong art is not a correction.
  */
-const VERSION = 'hm-67101adfd354'
+const VERSION = 'hm-d4ad00155bf7'
 const SHELL = `${VERSION}-shell`
 const RUNTIME = `${VERSION}-runtime`
 
